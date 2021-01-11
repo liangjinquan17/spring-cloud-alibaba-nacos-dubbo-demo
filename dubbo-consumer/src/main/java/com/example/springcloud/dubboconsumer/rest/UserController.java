a38d8fcbc57b780dd07cbd84e29857b45c2adcdf
liangjinquan <liangjinquan17@gmail.com>
2021-01-11 10:00:43 +0800

    feat:实现dubbo nacos 服务间调用

--- dubbo-consumer/src/main/java/com/example/springcloud/dubboconsumer/rest/UserController.java
+++ dubbo-consumer/src/main/java/com/example/springcloud/dubboconsumer/rest/UserController.java
@@ -1,13 +1,24 @@
 package com.example.springcloud.dubboconsumer.rest;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.example.springcloud.dubboremote.remote.IUserRemote;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+	@Reference
+	private IUserRemote userRemote;
+
 	@GetMapping("hello")
 	public String hello() {
 		return "hello dubboconsumer";
+	}
+
+	@GetMapping("getName")
+	public String getName(){
+		return "dubbo name:"+userRemote.getName();
 	}
 }
